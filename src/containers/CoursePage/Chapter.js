364e24550e3f07beaa07a253835e34b4615ea464
Dumbledore <ducanh.ng0502@gmail.com>
2019-09-28 11:48:40 +0700

    Render hexagon
    The hexagon present for the total of the lesson in a course

--- src/containers/CoursePage/Chapter.js
+++ src/containers/CoursePage/Chapter.js
@@ -11,6 +11,15 @@ class Chapter extends Component {
         })
         return result;
     }
+
+    renderHexagon = (length) => {
+        let result = [];
+        for (let i = 0; i < length; i++) {
+            result.push(<div key={i} className="hexagon_total"/>);
+        }
+        return result;
+    }
+
     render() {
         let { chapter,index } = this.props;
         return (
@@ -19,6 +28,10 @@ class Chapter extends Component {
                 <label data-toggle="collapse" data-target={"#toggle-" + index} className="test" aria-expanded="false" aria-controls={"toggle-" + index} >
                     <div className="nameChapter">
                         <p className="text">{chapter.name}</p>
+                        
+                    </div>
+                    <div className="hexa">
+                        {this.renderHexagon(chapter.totalLessons)}
                     </div>
                 </label>
                 <div className="titleChapter">
